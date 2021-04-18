e25a97711797d87a11dce3f950ff72a9691bac74
Daniel Sánchez Ceinos <daniel.ceinos@gmail.com>
2021-04-18 14:21:09 +0200

    Update send-webhook.py

--- send-webhook.py
+++ send-webhook.py
@@ -1,9 +1,10 @@
 # -*- coding: utf-8 -*-
 
 import requests
+import os
 import json, re
 
-webhookUrl = "https://webexapis.com/v1/webhooks/incoming/Y2lzY29zcGFyazovL3VzL1dFQkhPT0svYWQ3YmFmYzMtMDQ2Yy00YTRjLThlNmEtZjA5NWJiYjYzMzZj"
+webhookUrl = os.environ['WEBHOOK']
 
 headers = {'Content-type': 'application/json'}
 payload = {"markdown":"https://www.youtube.com/watch?v=1TewCPi92ro"}
