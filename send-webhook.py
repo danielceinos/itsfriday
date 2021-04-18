84cc133b5d6cb51b1a0d10976fc6a7744f6c5ebf
Daniel Sánchez Ceinos <daniel.ceinos@gmail.com>
2021-04-18 14:34:09 +0200

    Update send-webhook.py

--- send-webhook.py
+++ send-webhook.py
@@ -5,8 +5,7 @@ import os
 import json, re
 
 webhookUrl = os.environ['WEBHOOK']
-print(webhookUrl)
 
 headers = {'Content-type': 'application/json'}
-payload = {"markdown":"https://www.youtube.com/watch?v=1TewCPi92ro"}
+payload = {"markdown":"### It's friday then!!\nhttps://www.youtube.com/watch?v=1TewCPi92ro"}
 r = requests.post(webhookUrl, headers= headers, data=json.dumps(payload))
